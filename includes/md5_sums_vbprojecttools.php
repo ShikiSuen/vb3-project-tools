aa41dd74a0b70777ccad98d84b63d6490549c4d2
yrigaud <yrigaud>
2010-05-29 23:00:49 +0000

    Added 1 new file - MD5 file for AdminCP > Diagnostics. I will regenerate it when some PHP / JS / CSS files are added / edited / deleted.

---
+++ includes/md5_sums_vbprojecttools.php
@@ -0,0 +1,92 @@
+<?php
+// vbprojecttools 2.1.1
+$md5_sums = array(
+	'/' => array(
+		'project.php' => '470067ccdf119b979d9c02a2d2e61823',
+		'projectajax.php' => 'd6659e38a84d21021c1f6b393bc1a7a5',
+		'projectattachment.php' => '7ca50c93b7a3ff30f9c16421287ab8c5',
+		'projectexternal.php' => 'eb989cf106f72cff4d3b7bd1093b0f0b',
+		'projectmilestone.php' => '98786abfab556fbf254aa773fa643a8b',
+		'projectpost.php' => '62607855aca2c060a074c8a583ec99dd',
+		'projectsearch.php' => '1643d7af949265cde1bc95fcb0afc2f1',
+	),
+	'/admincp' => array(
+		'project.php' => '992894c19e49630388faec07f880b23e',
+		'projectattachment.php' => '65af956e18a448213253d84dd9b35b7d',
+		'projectpermission.php' => 'bb1d3c8dade9f985a63003288ad5ca35',
+	),
+	'/archive' => array(
+		'project.php' => '43bb9f4128f7550c7013dc1fd835a10d',
+	),
+	'/clientscript' => array(
+		'vbulletin_list_builder.js' => '77ef05aa1cca442ee4563669a67c452c',
+		'vbulletin_magic_select.css' => '58d0f938e8de726bd8082ef2f450ad9c',
+		'vbulletin_magic_select.js' => 'bec516dfdbf2c3082980d3accec47a64',
+		'vbulletin_pt_issue.js' => 'b39f459f434359c076d3b7f18664aecd',
+		'vbulletin_pt_read_marker.js' => '3f9632c133ef3a44f35f1f8f6521ba21',
+		'vbulletin_ptsearch_form.js' => '1a2c80edb1c7dfca06284bfa94df2fbe',
+	),
+	'/includes' => array(
+		'adminfunctions_projecttools.php' => '3a1aa7e93f3a03b21fca4dcf553107b9',
+		'class_bbcode_pt.php' => '6663e5d4b841223755ee0065ca6e05f1',
+		'class_dm_attachment_pt.php' => 'c041b7ef6c5054e6c5d90c654d07ec54',
+		'class_dm_pt_issue.php' => 'aef8e942d199adcc41d7d4b57939694c',
+		'class_dm_pt_issueassign.php' => '9e9b000542d4578f042d76730c3ed761',
+		'class_dm_pt_issuechange.php' => 'b465bff7c785cf4028a7069c0d65cb86',
+		'class_dm_pt_issuenote.php' => 'c42e703c101f9a100c59d362b1bcd1b4',
+		'class_dm_pt_issuepetition.php' => '8e84bc295479e5654cf1e8b66022b254',
+		'class_dm_pt_issuereport.php' => '180e8216f2a54f195de179ad0f4de3c3',
+		'class_dm_pt_issuestatus.php' => 'dfb7908aceb14856833b8fc4aec4b52f',
+		'class_dm_pt_issuesubscribe.php' => '142d50d191aafabb44e8488a24379380',
+		'class_dm_pt_issuetype.php' => 'c588849ce859eec33e56cda7fa6fe929',
+		'class_dm_pt_issuevote.php' => 'db93afeb477a5fed4b133b201a2b0c06',
+		'class_dm_pt_milestone.php' => '8ce4262d6f7b387f488aed5cea99698c',
+		'class_dm_pt_project.php' => '140cd09956a8a4f470ba85f6ede47a65',
+		'class_pt_issuelist.php' => '34efd22bf2ea840da5ab191ff29c22db',
+		'class_pt_issuenote.php' => '27ba167be56afbd01756ec4a4421bd47',
+		'class_pt_issuesearch.php' => '9bc42c3f62d749d02a0f1f33f43ce9f8',
+		'class_pt_patch_parse.php' => '7c7661a2db2e51f6b52e88dd611a9726',
+		'class_ptimporter.php' => '913db6075237cf95043c418b82f83677',
+		'class_reportitem_pt.php' => '4c2711cb5c87e8193d6940805047c621',
+		'class_upload_pt.php' => 'd44706ba1490a7024ea60164f04d170d',
+		'class_upload_ptimporter.php' => 'b98601919986a8c33437a3642069b5d9',
+		'functions_projecttools.php' => '326ce65a2f0a709f3f637a9c41525481',
+		'functions_pt_digest.php' => '8423c3e88e50b1e210b3baf8135d89c2',
+		'functions_pt_forumbit.php' => '968c137ce2b9a31d310359685a38a698',
+		'functions_pt_milestone.php' => 'ef2e53b8df40d771919ca4b0961947fd',
+		'functions_pt_notehistory.php' => '9980844d2848fbf7c750537de4e39233',
+		'functions_pt_patch.php' => 'f451c21a2c7b3627832908442ab7f162',
+		'functions_pt_posting.php' => '265a9b0399fea3286a313bdb52365468',
+		'functions_pt_search.php' => '9dd65327b15556aa279db7adae5ef08a',
+		'functions_pt_timeline.php' => 'da43dbc86874ebe88357a4830525f5ed',
+		'functions_pt_usercp.php' => '250170466ee9f66bc9ccb1b4be725021',
+		'functions_pt_usertools.php' => 'e5a343e69120c8fbb235c08c1c45d8a7',
+		'functions_ptimporter.php' => 'cd3b566da88ce8f64b99764b583ea22a',
+	),
+	'/includes/cron' => array(
+		'pt_cleanup.php' => '03a874002336dac2ad7d2d33d9de8ac0',
+		'pt_digestdaily.php' => '6282d57a2f246339d7459a6c17bf6cfb',
+		'pt_digestweekly.php' => '7fd9bcb338b9ea49cd9901f807d0caf3',
+	),
+	'/includes/xml' => array(
+		'bitfield_vbprojecttools.xml' => '32165347ac81d1807b5200eaa27ecddf',
+		'cpnav_vbprojecttools.xml' => 'b11204aed357b78e4ab31dd0c9b38d0c',
+		'hooks_vbprojecttools.xml' => 'ab3876ae66973c4d737cd36b5c8b67bb',
+		'product-vbprojecttools.xml' => '664ed272c2935375ee40915ff0d95152',
+	),
+	'/packages/vbprojecttools/search/indexcontroller' => array(
+		'issue.php' => '08e675d6c5914b84e5392d058bd6ed76',
+		'project.php' => 'c5682484c8b9ddfcf3dad206ad36617f',
+	),
+	'/packages/vbprojecttools/search/result' => array(
+		'issue.php' => '30186db87c9073bec379e55cdc5424f0',
+		'project.php' => '15a35951c62696b886c8c0348ac74e44',
+	),
+	'/packages/vbprojecttools/search/type' => array(
+		'issue.php' => '8a9433cb13154939e5e45304522404a4',
+		'project.php' => 'db679c370f269e73fa18fdec4f8025f0',
+	),
+);
+$md5_sum_softwareid = 'vbprojecttools';
+$md5_sum_versions['vbprojecttools'] = '2.1.1';
+?>
