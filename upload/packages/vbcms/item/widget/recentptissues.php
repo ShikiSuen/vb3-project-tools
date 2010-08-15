eea63ad6fc9e4cf6911902958cbdb65827f17c71
yrigaud <yrigaud>
2010-08-15 12:00:58 +0000

    Added 2 files in some specific directory related to CMS widget - Latest PT issues

---
+++ upload/packages/vbcms/item/widget/recentptissues.php
@@ -0,0 +1,64 @@
+<?php if (!defined('VB_ENTRY')) die('Access denied.');
+/*======================================================================*\
+|| #################################################################### ||
+|| #                  vBulletin Project Tools 2.1.2                   # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2010 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file is part of vBulletin Project Tools and subject to terms# ||
+|| #               of the vBulletin Open Source License               # ||
+|| # ---------------------------------------------------------------- # ||
+|| #    http://www.vbulletin.org/open_source_license_agreement.php    # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+/**
+ * Test Widget Item
+ *
+ * @package vBulletin
+ * @author Edwin Brown, vBulletin Development Team
+ * @version $Revision: 35393 $
+ * @since $Date: 2010-02-11 14:43:34 -0800 (Thu, 11 Feb 2010) $
+ * @copyright vBulletin Solutions Inc.
+ */
+class vBCms_Item_Widget_RecentPTIssues extends vBCms_Item_Widget
+{
+	/*Properties====================================================================*/
+
+	/**
+	 * A package identifier.
+	 * This is used to resolve any related class names.
+	 * It is also used by client code to resolve the class name of this widget.
+	 *
+	 * @var string
+	 */
+	protected $package = 'vBCms';
+
+	/**
+	 * A class identifier.
+	 * This is used to resolve any related class names.
+	 * It is also used by client code to resolve the class name of this widget.
+	 *
+	 * @var string
+	 */
+	protected $class = 'RecentPTIssues';
+
+	/** The default configuration **/
+	protected $config = array(
+		'template_name' => 'vbcms_widget_recentptissues_page',
+		'projectid' => 1,
+		'days' => 7,
+		'count' => 6,
+		'messagemaxchars' => 200,
+		'cache_ttl' => 5
+	);
+
+}
+
+/*======================================================================*\
+|| ####################################################################
+|| # Downloaded: 17:08, Fri Aug 6th 2010
+|| # SVN: $Revision: 35393 $
+|| ####################################################################
+\*======================================================================*/
+
+?>
