b743a0291784aefe7eb6fb463dad9b71af57dc3e
yrigaud <yrigaud>
2010-09-01 10:18:57 +0000

    Added files related to 2.0.1 - bug fixes will be in /branches/2.0/

---
+++ upload/clientscript/vbulletin_pt_issue.js
@@ -0,0 +1,34 @@
+/*======================================================================*\
+|| #################################################################### ||
+|| #                  vBulletin Project Tools 2.0.1                   # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2010 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file is part of vBulletin Project Tools and subject to terms# ||
+|| #               of the vBulletin Open Source License               # ||
+|| # ---------------------------------------------------------------- # ||
+|| #    http://www.vbulletin.org/open_source_license_agreement.php    # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+function issueattach_submit()
+{
+	if (!vB_Editor || !vB_Editor['vB_Editor_QR'])
+	{
+		// nothing to check against
+		return true;
+	}
+
+	if (stripcode(vB_Editor['vB_Editor_QR'].get_editor_contents(), vB_Editor['vB_Editor_QR'].wysiwyg_mode) != '')
+	{
+		if (confirm(vbphrase['reply_text_sure_submit_attach']))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ upload/includes/cron/pt_cleanup.php
@@ -0,0 +1,64 @@
+<?php
+/*======================================================================*\
+|| #################################################################### ||
+|| #                  vBulletin Project Tools 2.0.1                   # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2010 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file is part of vBulletin Project Tools and subject to terms# ||
+|| #               of the vBulletin Open Source License               # ||
+|| # ---------------------------------------------------------------- # ||
+|| #    http://www.vbulletin.org/open_source_license_agreement.php    # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+// ######################## SET PHP ENVIRONMENT ###########################
+error_reporting(E_ALL & ~E_NOTICE);
+if (!is_object($vbulletin->db))
+{
+	exit;
+}
+
+// note hashes are only valid for 5 minutes
+$vbulletin->db->query_write("
+	DELETE FROM " . TABLE_PREFIX . "pt_issuenotehash
+	WHERE dateline < " . (TIMENOW - 300)
+);
+
+$mysqlversion = $vbulletin->db->query_first("SELECT version() AS version");
+define('MYSQL_VERSION', $mysqlversion['version']);
+
+//searches expire after one hour
+if (version_compare(MYSQL_VERSION, '4.1.0', '>='))
+{
+	$vbulletin->db->query_write("
+		DELETE issuesearch, issuesearchresult
+		FROM " . TABLE_PREFIX . "pt_issuesearch AS issuesearch
+		LEFT JOIN " . TABLE_PREFIX . "pt_issuesearchresult AS issuesearchresult ON (issuesearchresult.issuesearchid = issuesearch.issuesearchid)
+		WHERE issuesearch.dateline < " . (TIMENOW - 3600)
+	);
+}
+else
+{
+	$vbulletin->db->query_write("
+		DELETE " . TABLE_PREFIX . "pt_issuesearch, " . TABLE_PREFIX . "pt_issuesearchresult
+		FROM " . TABLE_PREFIX . "pt_issuesearch AS issuesearch
+		LEFT JOIN " . TABLE_PREFIX . "pt_issuesearchresult AS issuesearchresult ON (issuesearchresult.issuesearchid = issuesearch.issuesearchid)
+		WHERE issuesearch.dateline < " . (TIMENOW - 3600)
+	);
+}
+
+// remove old issue read marking data
+$vbulletin->db->query_write("
+	DELETE FROM " . TABLE_PREFIX . "pt_issueread
+	WHERE readtime < " . (TIMENOW - ($vbulletin->options['markinglimit'] * 86400))
+);
+
+// remove old project read marking data
+$vbulletin->db->query_write("
+	DELETE FROM " . TABLE_PREFIX . "pt_projectread
+	WHERE readtime < " . (TIMENOW - ($vbulletin->options['markinglimit'] * 86400))
+);
+
+log_cron_action('', $nextitem, 1);
+
+?>
