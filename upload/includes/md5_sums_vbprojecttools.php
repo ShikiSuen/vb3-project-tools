5a3456728c1b51ea2bcd3754c9b972e10644b30f
yrigaud <yrigaud>
2010-11-07 21:00:15 +0000

    Fixed issues #147 & #148.

--- upload/includes/md5_sums_vbprojecttools.php
+++ upload/includes/md5_sums_vbprojecttools.php
@@ -75,7 +75,7 @@ $md5_sums = array(
 		'cpnav_vbprojecttools.xml' => 'b11204aed357b78e4ab31dd0c9b38d0c',
 		'cssrollup_vbprojecttools.xml' => '65b969152897feff010c66cde098bce3',
 		'hooks_vbprojecttools.xml' => 'ab3876ae66973c4d737cd36b5c8b67bb',
-		'product-vbprojecttools.xml' => '396154f78358d3e263787d2d1e3eb9df',
+		'product-vbprojecttools.xml' => '0fd81219e52a2b501cec1da2cb5a469d',
 	),
 	'/packages/vbcms/item/widget' => array(
 		'recentptcomments.php' => 'c184eac2d54ea4f8295a2a9f229d007e',
