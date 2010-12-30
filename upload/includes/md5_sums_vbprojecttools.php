7ed7752683feddc0591a4158682b7ba3b961055e
yrigaud <yrigaud>
2010-12-30 00:14:49 +0000

    Added latest 2.1 into 'branches'.

---
+++ upload/includes/md5_sums_vbprojecttools.php
@@ -0,0 +1,113 @@
+<?php
+// vbprojecttools 2.1.2
+$md5_sums = array(
+	'/' => array(
+		'project.php' => 'd0aefadcd86f2fe523e1054a105702c3',
+		'projectajax.php' => 'd23f99014bc25c958af27f59a7baa768',
+		'projectattachment.php' => '04d600f686b4da85c1ff487a2cf0187b',
+		'projectexternal.php' => '447a7f2c65fe764913b9239386fdd633',
+		'projectmilestone.php' => '1c0b10361326c7f5dbe45e12d2ac7e34',
+		'projectpost.php' => 'b460d380f9c4e884f7618cf9382b37c5',
+		'projectsearch.php' => '4ced26af853f560c22afdfe61d2f3218',
+	),
+	'/admincp' => array(
+		'project.php' => 'd8aab447be50a6c01a6db35cdeb9f0f8',
+		'projectattachment.php' => 'c20beca840f50c46d844bb0e4d967f5a',
+		'projectpermission.php' => 'cc325b8bd5c8ec2939237143e42421f8',
+	),
+	'/archive' => array(
+		'project.php' => '3a20c256fad7e6c0b3256a362c7c4f64',
+	),
+	'/clientscript' => array(
+		'vbulletin_list_builder.js' => 'a6f6e4c9174fb16eeb2ffb163590f622',
+		'vbulletin_magic_select.js' => '5a071c41c3c0eaee504a34b28e72c866',
+		'vbulletin_pt_issue.js' => '357ba0abece7d67dbaf725ab0ad9f4ae',
+		'vbulletin_pt_read_marker.js' => '9923b22e8c315613760f5ac3491573e5',
+		'vbulletin_ptsearch_form.js' => 'd84e84770b72a3c9fac93ed7c5dbc47f',
+	),
+	'/includes' => array(
+		'adminfunctions_projecttools.php' => 'f0df09f2283de4245c9302a7905d25a1',
+		'class_bbcode_pt.php' => '2bfe1e89b944d1bdf2ac2d1654848e88',
+		'class_dm_attachment_pt.php' => '1486ae1535424df296464eef4968adef',
+		'class_dm_pt_issue.php' => 'f551731b9bc22fcd08b44147b5a701c9',
+		'class_dm_pt_issueassign.php' => '8d1640188e840263545287b8ceb69e25',
+		'class_dm_pt_issuechange.php' => '2f7e0d0657085cf02f0377a0e1d6e379',
+		'class_dm_pt_issuenote.php' => 'a8813917c94de0a38692cab24abe41ec',
+		'class_dm_pt_issuepetition.php' => '5687db68042ab7affd3d04ff42c08426',
+		'class_dm_pt_issuereport.php' => '8a0a238ef9b7a856fe37915e905b227a',
+		'class_dm_pt_issuestatus.php' => 'c5b5d424a63ceae408236bc8ba2113cf',
+		'class_dm_pt_issuesubscribe.php' => 'dd3e830feab2701592be07784e2268e4',
+		'class_dm_pt_issuetype.php' => 'e2fb5b24120f8298056727caa92dfbe4',
+		'class_dm_pt_issuevote.php' => '123a2b01318917b96e1c68777d2f1b03',
+		'class_dm_pt_milestone.php' => 'afbca5b634550995b7f3f1ba81e3f0ad',
+		'class_dm_pt_project.php' => 'd820cb7a77f1be0b25154b4b2befa497',
+		'class_pt_issuelist.php' => '42b0df9a8d32251438a5feea3a8a8135',
+		'class_pt_issuenote.php' => '030e3104b75316f193cc52ee7aef23f9',
+		'class_pt_issuesearch.php' => 'e3400c5a2674cff68b37a38c8c33ad2d',
+		'class_pt_patch_parse.php' => '42320688682f8d6df89450faa3493a3b',
+		'class_ptimporter.php' => '5405349199ebeeb2542948aa01c7d108',
+		'class_reportitem_pt.php' => '74214ded73a9e45de79458517895bfad',
+		'class_upload_pt.php' => 'd9a9fb27f78fdaccdbe32992fbfad64c',
+		'class_upload_ptimporter.php' => 'e87d6e94a44ba1b825f1b6e7c25170bc',
+		'functions_projecttools.php' => 'eefb5437970971e35961e918d37358db',
+		'functions_pt_digest.php' => '77d4a1a0ed0a5d36c71803069a5ab68f',
+		'functions_pt_forumbit.php' => '7d9f1a4bd40a6b281c849ac80d9ad5f3',
+		'functions_pt_milestone.php' => 'bc815b66bb6fdc0ccfcf90468df54f5c',
+		'functions_pt_notehistory.php' => '0afda2ebf9f263a143a8d80cda254d01',
+		'functions_pt_patch.php' => '0febc58be4c09a51a5685249c7462e97',
+		'functions_pt_posting.php' => '493fbbe5a2a3eafd9f5545a958289aea',
+		'functions_pt_search.php' => '48110f919a3bc91eb4be33905e5e263c',
+		'functions_pt_timeline.php' => '117d49b3e5507623e4a3b0775d6ba129',
+		'functions_pt_usercp.php' => '00a5adad40cee34aef310e1505b153c2',
+		'functions_pt_usertools.php' => '633af292a0d285de6e7c78a1b5abd33e',
+		'functions_ptimporter.php' => '2c69a40a5be644b369dd86c77ead2dd4',
+	),
+	'/includes/block' => array(
+		'ptissues.php' => '3087daa3db091302d98c45d050c88616',
+	),
+	'/includes/cron' => array(
+		'pt_cleanup.php' => '5b62b023dc0da834e2adaf89d14d0c14',
+		'pt_digestdaily.php' => '274aa63fd3a9592d5c50e07e9dc4645a',
+		'pt_digestweekly.php' => 'bb804e30e719ae21efc34223d23584b6',
+	),
+	'/includes/xml' => array(
+		'bitfield_vbprojecttools.xml' => '293cc0eb3f6fbd2d9aad308710cf7871',
+		'cpnav_vbprojecttools.xml' => 'b11204aed357b78e4ab31dd0c9b38d0c',
+		'cssrollup_vbprojecttools.xml' => '65b969152897feff010c66cde098bce3',
+		'hooks_vbprojecttools.xml' => 'ab3876ae66973c4d737cd36b5c8b67bb',
+		'product-vbprojecttools.xml' => 'f4bba4276c4a417b01b64038597652be',
+	),
+	'/packages/vbcms/item/widget' => array(
+		'recentptcomments.php' => 'c184eac2d54ea4f8295a2a9f229d007e',
+		'recentptissues.php' => '5a6fc67f171657305b3ac0d7db9baaa0',
+	),
+	'/packages/vbcms/widget' => array(
+		'recentptcomments.php' => 'c945cd9d5a2719ac9050bbea19b4e1c4',
+		'recentptissues.php' => '548f0cb98668ef3d37af8231035e3257',
+	),
+	'/packages/vbprojecttools/search/indexcontroller' => array(
+		'issuenote.php' => 'f313ef3c92b6d881730294c6354bf022',
+		'project.php' => '7dac6dd6b7dfe66d528b62b41a63ad10',
+	),
+	'/packages/vbprojecttools/search/result' => array(
+		'issue.php' => '719b99677e0094600d5cfdb843908859',
+		'issuenote.php' => 'ed5e4a710b81707daa435edbbef53682',
+		'project.php' => '178d3759bd0696c7ed1aaaf1a9499286',
+	),
+	'/packages/vbprojecttools/search/searchcontroller' => array(
+		'newissuenote.php' => '6b2edf229df3a8f7d536ea804c21a74d',
+	),
+	'/packages/vbprojecttools/search/type' => array(
+		'issue.php' => '121d8bcecb10f93bd7a3298bbc910e35',
+		'issuenote.php' => '655bc6d1573d29725b8e2adfac8ed847',
+		'project.php' => 'dc7a39db478af52226986fc470c6f4db',
+	),
+	'/vb/legacy' => array(
+		'issue.php' => 'a70107c2017f43d744ea36ca9dc25c5a',
+		'issuenote.php' => '677886071c79456cf8f11bc7d932a212',
+		'project.php' => '3291ff9d062079af50933797e2c8188b',
+	),
+);
+$md5_sum_softwareid = 'vbprojecttools';
+$md5_sum_versions['vbprojecttools'] = '2.1.2';
+?>
---
+++ upload/packages/vbprojecttools/search/result/issue.php
@@ -0,0 +1,215 @@
+<?php if (!defined('VB_ENTRY')) die('Access denied.');
+
+/*======================================================================*\
+|| #################################################################### ||
+|| #                  vBulletin Project Tools 2.1.2                   # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2010 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file is part of vBulletin Project Tools and subject to terms# ||
+|| #               of the vBulletin Open Source License               # ||
+|| # ---------------------------------------------------------------- # ||
+|| #    http://www.vbulletin.org/open_source_license_agreement.php    # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+/**
+ * @package vBulletin Project Tools
+ * @subpackage Search
+ * @author $Author$
+ * @version $Revision$
+ * @since $Date$
+ * @copyright http://www.vbulletin.org/open_source_license_agreement.php
+ */
+
+require_once(DIR . '/includes/functions_projecttools.php');
+require_once(DIR . '/vb/search/result.php');
+
+/**
+ * Enter description here...
+ *
+ * @package vBulletin Project Tools
+ * @subpackage Search
+ */
+class vBProjectTools_Search_Result_Issue extends vB_Search_Result
+{
+	public static function create($id)
+	{
+		require_once(DIR . '/vb/legacy/issue.php');
+
+		if ($issue = vB_Legacy_Issue::create_from_id($id))
+		{
+			$item = new vBProjectTools_Search_Result_Issue();
+			$item->issue = $issue;
+			return $item;
+		}
+
+		// If we get here, the id must be invalid
+		require_once(DIR . '/vb/search/result/null.php');
+		return new vB_Search_Result_Null();
+	}
+
+	public static function create_from_issue($issue)
+	{
+		if ($issue)
+		{
+			$item = new vBProjectTools_Search_Result_Issue();
+
+			// If we just have an id, we need to create the
+			// object
+			$item->issue = $issue;
+			return $item;
+		}
+		else
+		{
+			require_once(DIR . '/vb/search/result/null.php');
+			return new vB_Search_Result_Null();
+		}
+	}
+
+	protected function __construct() {}
+
+	public function get_contenttype()
+	{
+		return vB_Search_Core::get_instance()->get_contenttypeid('vBProjectTools', 'Issue');
+	}
+
+	public function can_search($user)
+	{
+		return $this->issue->can_search($user);
+	}
+
+	public function render($current_user, $criteria, $template_name = '')
+	{
+		global $vbulletin, $vbphrase, $show;
+
+		$phrase = new vB_Legacy_Phrase();
+		$phrase->add_phrase_groups(array('projecttools'));
+
+		if (!strlen($template_name))
+		{
+			$template_name = 'search_results_ptissue';
+		}
+
+		$datastores = $vbulletin->db->query_read("
+			SELECT data, title
+			FROM " . TABLE_PREFIX . "datastore
+			WHERE title IN('pt_bitfields', 'pt_permissions', 'pt_issuestatus', 'pt_issuetype', 'pt_projects', 'pt_categories', 'pt_assignable', 'pt_versions')
+		");
+
+		while ($datastore = $vbulletin->db->fetch_array($datastores))
+		{
+			$title = $datastore['title'];
+
+			if (!is_array($datastore['data']))
+			{
+				$data = unserialize($datastore['data']);
+
+				if (is_array($data))
+				{
+					$vbulletin->$title = $data;
+				}
+			}
+			else if ($datastore['data'] != '')
+			{
+				$vbulletin->$title = $datastore['data'];
+			}
+		}
+
+		if (!$search_perms = build_issue_permissions_query($vbulletin->userinfo, 'cansearch'))
+		{
+			print_no_permission();
+		}
+
+		($hook = vBulletinHook::fetch_hook('projectsearch_results_start')) ? eval($hook) : false;
+
+		if (!$vbulletin->GPC['pagenumber'])
+		{
+			$vbulletin->GPC['pagenumber'] = 1;
+		}
+		if (!$vbulletin->GPC['start'])
+		{
+			$vbulletin->GPC['start'] = ($vbulletin->GPC['pagenumber'] - 1) * $perpage;
+		}
+
+		if (!$perpage)
+		{
+			$perpage = 999999;
+		}
+
+		build_issue_private_lastpost_sql_all($vbulletin->userinfo, $private_lastpost_join, $private_lastpost_fields);
+
+		$replycount_clause = fetch_private_replycount_clause($vbulletin->userinfo);
+
+		$show['first_group'] = true;
+		$resultgroupbits = '';
+
+		$hook_query_fields = $hook_query_joins = $hook_query_where = '';
+		($hook = vBulletinHook::fetch_hook('projectsearch_results_query')) ? eval($hook) : false;
+
+		$results = $vbulletin->db->query_first("
+			SELECT issue.*
+				" . ($vbulletin->userinfo['userid'] ? ", issuesubscribe.subscribetype, IF(issueassign.issueid IS NULL, 0, 1) AS isassigned" : '') . "
+				" . ($marking ? ", issueread.readtime AS issueread, projectread.readtime AS projectread" : '') . "
+				" . ($private_lastpost_fields ? ", $private_lastpost_fields" : '') . "
+				" . ($replycount_clause ? ", $replycount_clause AS replycount" : '') . "
+				$hook_query_fields
+			FROM " . TABLE_PREFIX . "pt_issue AS issue
+			" . ($vbulletin->userinfo['userid'] ? "
+				LEFT JOIN " . TABLE_PREFIX . "pt_issuesubscribe AS issuesubscribe ON (issuesubscribe.issueid = issue.issueid AND issuesubscribe.userid = " . $vbulletin->userinfo['userid'] . ")
+				LEFT JOIN " . TABLE_PREFIX . "pt_issueassign AS issueassign ON (issueassign.issueid = issue.issueid AND issueassign.userid = " . $vbulletin->userinfo['userid'] . ")
+			" : '') . "
+			" . ($marking ? "
+				LEFT JOIN " . TABLE_PREFIX . "pt_issueread AS issueread ON (issueread.issueid = issue.issueid AND issueread.userid = " . $vbulletin->userinfo['userid'] . ")
+				LEFT JOIN " . TABLE_PREFIX . "pt_projectread AS projectread ON (projectread.projectid = issue.projectid AND projectread.userid = " . $vbulletin->userinfo['userid'] . " AND projectread.issuetypeid = issue.issuetypeid)
+			" : '') . "
+			$private_lastpost_join
+			$hook_query_joins
+			WHERE issue.issueid = " . $this->issue['issueid'] . "
+				AND ((" . implode(') OR (', $search_perms) . "))
+				$hook_query_where
+			LIMIT $perpage
+		");
+
+		static $projectperms = array();
+
+		$issue = $this->get_issue($results['issueid']);
+
+		if (!isset($projectperms["$issue[projectid]"]))
+		{
+			$projectperms["$issue[projectid]"] = fetch_project_permissions($vbulletin->userinfo, $issue['projectid']);
+		}
+
+		$project = $vbulletin->pt_projects["$issue[projectid]"];
+		$issueperms = $projectperms["$issue[projectid]"]["$issue[issuetypeid]"];
+		$posting_perms = prepare_issue_posting_pemissions($issue, $issueperms);
+
+		$show['edit_issue'] = $posting_perms['issue_edit'];
+		$show['status_edit'] = $posting_perms['status_edit'];
+
+		$issue = prepare_issue($issue);
+
+		($hook = vBulletinHook::fetch_hook('projectsearch_results_bit')) ? eval($hook) : false;
+
+		$template = vB_Template::create($template_name);
+			$template->register('issue', $issue);
+			$template->register('project', $project);
+		return $template->render();
+	}
+
+	private function get_issue($id)
+	{
+		global $vbulletin;
+
+		$issue = $vbulletin->db->query_first("
+			SELECT *
+			FROM " . TABLE_PREFIX . "pt_issue
+			WHERE issueid = $id
+		");
+
+		return $issue;
+	}
+
+	private $issue;
+}
+
+?>
