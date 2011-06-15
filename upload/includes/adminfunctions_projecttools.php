e50f2ab0d2485579fb063a9c12bdf090b3029624
yrigaud <yrigaud>
2011-06-15 09:34:36 +0000

    Added the fetch_scriptpath() function back only for PT (removed from core code).

--- upload/includes/adminfunctions_projecttools.php
+++ upload/includes/adminfunctions_projecttools.php
@@ -558,4 +558,67 @@ function build_pt_user_list($name, $callback)
 	return $userlist;
 }
 
+/**
+* Fetches the 'scriptpath' variable - ie: the URI of the current page
+*
+* @return	string
+*/
+function fetch_scriptpath()
+{
+	global $vbulletin;
+
+	if ($vbulletin->scriptpath != '')
+	{
+		return $vbulletin->scriptpath;
+	}
+	else
+	{
+		if ($_SERVER['REQUEST_URI'] OR $_ENV['REQUEST_URI'])
+		{
+			$scriptpath = $_SERVER['REQUEST_URI'] ? $_SERVER['REQUEST_URI'] : $_ENV['REQUEST_URI'];
+		}
+		else
+		{
+			if ($_SERVER['PATH_INFO'] OR $_ENV['PATH_INFO'])
+			{
+				$scriptpath = $_SERVER['PATH_INFO'] ? $_SERVER['PATH_INFO'] : $_ENV['PATH_INFO'];
+			}
+			else if ($_SERVER['REDIRECT_URL'] OR $_ENV['REDIRECT_URL'])
+			{
+				$scriptpath = $_SERVER['REDIRECT_URL'] ? $_SERVER['REDIRECT_URL'] : $_ENV['REDIRECT_URL'];
+			}
+			else
+			{
+				$scriptpath = $_SERVER['PHP_SELF'] ? $_SERVER['PHP_SELF'] : $_ENV['PHP_SELF'];
+			}
+
+			if ($_SERVER['QUERY_STRING'] OR $_ENV['QUERY_STRING'])
+			{
+				$scriptpath .= '?' . ($_SERVER['QUERY_STRING'] ? $_SERVER['QUERY_STRING'] : $_ENV['QUERY_STRING']);
+			}
+		}
+
+		// in the future we should set $registry->script here too
+		$quest_pos = strpos($scriptpath, '?');
+		if ($quest_pos !== false)
+		{
+			$script = urldecode(substr($scriptpath, 0, $quest_pos));
+			$scriptpath = $script . substr($scriptpath, $quest_pos);
+		}
+		else
+		{
+			$scriptpath = urldecode($scriptpath);
+		}
+
+		// store a version that includes the sessionhash
+		$vbulletin->reloadurl = $vbulletin->input->xss_clean($scriptpath);
+
+		$scriptpath = $vbulletin->input->strip_sessionhash($scriptpath);
+		$scriptpath = $vbulletin->input->xss_clean($scriptpath);
+		$vbulletin->scriptpath = $scriptpath;
+
+		return $scriptpath;
+	}
+}
+
 ?>
