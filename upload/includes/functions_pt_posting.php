f003783a545beefe5175c215936b550daef6187b
yrigaud <yrigaud>
2011-07-29 00:03:27 +0000

    Fixes part of #213.

--- upload/includes/functions_pt_posting.php
+++ upload/includes/functions_pt_posting.php
@@ -3,7 +3,7 @@
 || #################################################################### ||
 || #                  vBulletin Project Tools 2.2.0                   # ||
 || # ---------------------------------------------------------------- # ||
-|| # Copyright ©2000-2011 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # Copyright ï¿½2000-2011 vBulletin Solutions Inc. All Rights Reserved. ||
 || # This file is part of vBulletin Project Tools and subject to terms# ||
 || #               of the vBulletin Open Source License               # ||
 || # ---------------------------------------------------------------- # ||
