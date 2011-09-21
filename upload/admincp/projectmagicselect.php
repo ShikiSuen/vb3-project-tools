e092909a0573ad526f768969f82b57a3f1086377
yrigaud <yrigaud>
2011-09-21 20:11:15 +0000

    Added a new file for issue #212.

---
+++ upload/admincp/projectmagicselect.php
@@ -0,0 +1,61 @@
+<?php
+/*======================================================================*\
+|| #################################################################### ||
+|| #                  vBulletin Project Tools 2.2.0                   # ||
+|| # ---------------------------------------------------------------- # ||
+|| # Copyright ©2000-2011 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # This file is part of vBulletin Project Tools and subject to terms# ||
+|| #               of the vBulletin Open Source License               # ||
+|| # ---------------------------------------------------------------- # ||
+|| #    http://www.vbulletin.org/open_source_license_agreement.php    # ||
+|| #################################################################### ||
+\*======================================================================*/
+
+// ####################### SET PHP ENVIRONMENT ###########################
+error_reporting(E_ALL & ~E_NOTICE);
+
+// ##################### DEFINE IMPORTANT CONSTANTS #######################
+define('CVS_REVISION', '$Rev$');
+
+// #################### PRE-CACHE TEMPLATES AND DATA ######################
+$phrasegroups = array(
+	'projecttools',
+	'projecttoolsadmin'
+);
+
+$specialtemplates = array();
+
+// ########################## REQUIRE BACK-END ############################
+require_once('./global.php');
+
+if (empty($vbulletin->products['vbprojecttools']))
+{
+	print_stop_message('product_not_installed_disabled');
+}
+
+require_once(DIR . '/includes/adminfunctions_projecttools.php');
+require_once(DIR . '/includes/functions_projecttools.php');
+
+if (!function_exists('ini_size_to_bytes') OR (($current_memory_limit = ini_size_to_bytes(@ini_get('memory_limit'))) < 128 * 1024 * 1024 AND $current_memory_limit > 0))
+{
+	@ini_set('memory_limit', 128 * 1024 * 1024);
+}
+
+$full_product_info = fetch_product_list(true);
+
+// ######################## CHECK ADMIN PERMISSIONS #######################
+if (!can_administer('canpt'))
+{
+	print_cp_no_permission();
+}
+
+// ############################# LOG ACTION ###############################
+log_admin_action();
+
+// ########################################################################
+// ######################### START MAIN SCRIPT ############################
+// ########################################################################
+
+
+
+?>
