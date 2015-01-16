d61117ba6dd69e3cdbe29fb164958c063e84587f
yrigaud <yrigaud>
2015-01-16 17:39:51 +0000

    Fixed issue #354.

--- vb4/upload/clientscript/vbulletin_pt_petition.js
+++ vb4/upload/clientscript/vbulletin_pt_petition.js
@@ -2,7 +2,7 @@
 || #################################################################### ||
 || #                  vBulletin Project Tools 2.2.2                   # ||
 || # ---------------------------------------------------------------- # ||
-|| # Copyright ©2000-2014 vBulletin Solutions Inc. All Rights Reserved. ||
+|| # Copyright ©2000-2015 vBulletin Solutions Inc. All Rights Reserved. ||
 || # This file is part of vBulletin Project Tools and subject to terms# ||
 || #               of the vBulletin Open Source License               # ||
 || # ---------------------------------------------------------------- # ||
